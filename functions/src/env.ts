35399dc98ec4145d76d43da77d28c0ddd2a6263d
kibet <kibet@domain.local>
2021-01-22 15:57:07 +0300

    - setup env variables configs - add a way to verify env configs

---
+++ functions/src/env.ts
@@ -0,0 +1,26 @@
+import * as functions from "firebase-functions";
+
+type EnvConfig = Record<string, unknown>;
+
+const newsletterConfig = functions.config().newsletter;
+
+const isValidConfig = (val: unknown): val is EnvConfig =>
+  typeof val === "object" && val != null;
+
+if (!isValidConfig(newsletterConfig)) {
+  throw new Error("Invalid environment variable config");
+}
+
+const isString = (val: unknown): val is string => typeof val === "string";
+
+const verifyEnv = (envName: keyof EnvConfig): string => {
+  const value = newsletterConfig[envName];
+
+  if (!isString(value)) {
+    throw new Error(`Invalid '${envName}' variable ${value}`);
+  }
+
+  return value;
+};
+
+export const PLACEHOlDER = verifyEnv("placeholder");
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import { PLACEHOlDER } from "./env";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+  functions.logger.info("Hello logs!", { structuredData: true });
+
+  console.log(PLACEHOlDER);
   response.send("Hello from Firebase!");
 });
