99abf548fe5f2f6ad5e3790330899474f3317e17
kibet <kibet@domain.local>
2021-01-23 13:16:02 +0300

    - refactor and remove repetitive code - make it more modular by abstracting data and request creation

--- functions/src/email.ts
+++ functions/src/email.ts
@@ -3,54 +3,67 @@ import * as FormData from "form-data";
 import { EMAIL_SENDER_DOMAIN, MAILGUN_API_KEY } from "./env";
 import { EmailApiOutcome, intoEmailOutcome } from "./email-utils";
 
-const NEWSLETTER_NAME = "newsletter";
 const BASE_URL = "https://api.mailgun.net/v3/";
+const NEWSLETTER_NAME = "newsletter";
+const MAILING_LIST = `${NEWSLETTER_NAME}@${EMAIL_SENDER_DOMAIN}`;
+
+const createEndpoint = (path: string) => `${BASE_URL}${path}`;
+
+type Operation = "subscribe" | "unsubscribe";
+
+interface MailgunApiData {
+  endpoint: string;
+  form: FormData;
+  httpMethod: "post" | "put";
+}
+
+const createMailgunApiData = (op: Operation, email: string): MailgunApiData => {
+  const form = new FormData();
+
+  if (op === "subscribe") {
+    form.append("subscribed", "yes");
+    form.append("address", email);
+
+    return {
+      form,
+      endpoint: createEndpoint(`lists/${MAILING_LIST}/members`),
+      httpMethod: "post",
+    };
+  } else {
+    form.append("subscribed", "no");
+
+    return {
+      form,
+      endpoint: createEndpoint(`lists/${MAILING_LIST}/members/${email}`),
+      httpMethod: "put",
+    };
+  }
+};
+
+const createRequest = (
+  op: Operation,
+  email: string
+): Promise<EmailApiOutcome> => {
+  const { form, endpoint, httpMethod } = createMailgunApiData(op, email);
+
+  return axios({
+    url: endpoint,
+    method: httpMethod,
+    data: form,
+    headers: form.getHeaders(),
+    auth: {
+      username: "api",
+      password: MAILGUN_API_KEY,
+    },
+  })
+    .then(() => EmailApiOutcome.Success)
+    .catch(intoEmailOutcome);
+};
 
 export const addSubscriberTomailingList = async (
   email: string
-): Promise<EmailApiOutcome> => {
-  const form = new FormData();
-
-  form.append("subscribed", "yes");
-  form.append("address", email);
-
-  const mailingList = `${NEWSLETTER_NAME}@${EMAIL_SENDER_DOMAIN}`;
-  const endpoint = `${BASE_URL}lists/${mailingList}/members`;
-
-  try {
-    await axios.post(endpoint, form, {
-      auth: {
-        username: "api",
-        password: MAILGUN_API_KEY,
-      },
-      headers: form.getHeaders(),
-    });
-    return EmailApiOutcome.Success;
-  } catch (err) {
-    return intoEmailOutcome(err);
-  }
-};
+): Promise<EmailApiOutcome> => createRequest("subscribe", email);
 
 export const unsubscribeUserFromMailingList = async (
   email: string
-): Promise<EmailApiOutcome> => {
-  const form = new FormData();
-
-  form.append("subscribed", "no");
-
-  const mailingList = `${NEWSLETTER_NAME}@${EMAIL_SENDER_DOMAIN}`;
-  const endpoint = `${BASE_URL}lists/${mailingList}/members/${email}`;
-
-  try {
-    await axios.put(endpoint, form, {
-      auth: {
-        username: "api",
-        password: MAILGUN_API_KEY,
-      },
-      headers: form.getHeaders(),
-    });
-    return EmailApiOutcome.Success;
-  } catch (err) {
-    return intoEmailOutcome(err);
-  }
-};
+): Promise<EmailApiOutcome> => createRequest("unsubscribe", email);
